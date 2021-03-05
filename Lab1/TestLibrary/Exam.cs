660ef1ae608a67ba24e97e72e18e751987b3787c
M0o4 <37911752+M0o4@users.noreply.github.com>
2021-03-05 19:34:12 +0300

    start making exam class

--- Lab1/TestLibrary/Exam.cs
+++ Lab1/TestLibrary/Exam.cs
@@ -4,10 +4,62 @@ namespace TestLibrary
 {
     public class Exam : Test
     {
+        public string SubjectName
+        {
+            get => _subjectName;
+            set => _subjectName = string.IsNullOrEmpty(value) ? "No subject name" : value;
+        }
+
+        public string SchoolName
+        {
+            get => _schoolName;
+            set => _schoolName = string.IsNullOrEmpty(value) ? "No school name" : value;
+        }
+
+        protected string _subjectName;
+        protected string _schoolName;
+
+        private void AddSubjectName()
+        {
+            Console.WriteLine("Введите название предмета.");
+            Console.Write("Ввод: ");
+
+            SubjectName = Console.ReadLine();
+        }
+
+        public override void AddQuestion()
+        {
+            AddSubjectName();
+
+            Console.WriteLine("Какие задания вы хотите добавить?");
+            Console.WriteLine("1)Тестовые\n2)Письменные");
+            
+            //switch
+            //1)Тестовые =>
+            base.AddQuestion();
+            //Добавить еще тест?
+            //да нет
+            //Добавить письменный
+            //код
+            _questions.Add(new Question("2+2", "4"));
+            //Добавить еще тест?
+            //да нет
+        }
+
         public override void AskQuestion()
         {
-            base.AskQuestion();
-            Console.Write("Ку");
+            //проверить какой тест
+            foreach (var question in _questions)
+            {
+                if(question.GetType() == typeof(TestQuestion))
+                {
+                    base.AskQuestion();
+                    continue;
+                }
+
+                //спросить письменный тест
+            }
+            
         }
     }
 }
